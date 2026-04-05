d768004f117e420d8d3742f8994c44126233c3ec
Kalpana Joyce Dvari <dovarikalpanajoyce@gmail.com>
2026-04-05 01:49:58 +0100

    feat: lol i made 5 mistakes- learning by making mistakes

--- src/main.zig
+++ src/main.zig
@@ -1,22 +1,14 @@
 const std = @import("std");
-
-pub fn main() void {
-    // var = mutable, const = immutable
-    const language = "Zig";
-    var count: u8 = 0;
-
-    std.debug.print("learning {s} at 5am like a nerd\n", .{language});
-
-    // while loop
-    while (count < 5) {
-        std.debug.print("count: {d}\n", .{count});
-        count += 1;
-    }
-
-    // if statement
-    if (count == 5) {
-        std.debug.print("counted to 5 successfully\n", .{});
-    } else {
-        std.debug.print("something went wrong\n", .{});
-    }
+pub fn main() void{
+    const a :i32= -10;
+    const b : u32= 10;
+    const c :u8 =255;
+    const pi:f64=3.141;
+    const is_nerd: bool=true;
+    const name: []const u8 = "Joy";  // need the const keyword in the type
+     std.debug.print("a={d} b={d} c={d}\n", .{a, b, c});
+    std.debug.print("pi={d}\n", .{pi});
+    std.debug.print("is_nerd={}\n", .{is_nerd});
+    std.debug.print("name={s}\n", .{name});
 }
+
